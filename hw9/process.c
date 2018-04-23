23c357567aa86f76661f148081354929c9962058
JasonJiangCN <jx240230@gmail.com>
2018-04-22 21:22:49 -0500

    reviesed data

--- hw9/process.c
+++ hw9/process.c
@@ -5,17 +5,16 @@ int main(){
     char *d = malloc(4);
     *d  = ' '; 
     FILE* fp = fopen("data.txt", "r");
-    char str[10];
-
-    while (fgets(str, 10, fp) != NULL){
-        if (str[8] == '\n');
-        str[8] = '\0';
-        if (str[9] == '\n');
-        str[9] = '\0';
-        if (str[7] == '\n');
-        str[7] = '\0';
-
-
+    char *str = NULL;
+    size_t len = 0;
+    ssize_t nread;
+    while ((nread = getline(&str, &len, fp)) != -1) {
+        if (str[8] == '\n')
+            str[8] = '\0';
+        if (str[9] == '\n')
+            str[9] = '\0';
+        if (str[7] == '\n')
+            str[7] = '\0';
         char* a = strtok(str, d);
         char* b = strtok(NULL, d);
         printf("put(%s, %s);",a, b);
